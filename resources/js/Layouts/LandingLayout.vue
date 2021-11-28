6832e22dd9b65420471e69e64d8626eb2042b065
sergeykotenko <sergeykotenko@bk.ru>
2021-11-28 19:09:59 +0300

    new project

---
+++ resources/js/Layouts/LandingLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+        <div class="row justify-content-center">
+
+<!-- page content -->
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/components/TechnologyTreeApp.vue
@@ -0,0 +1,11 @@
+<template>
+<h2>Technology Tree</h2>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
