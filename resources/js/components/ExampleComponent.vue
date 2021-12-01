8cefb20a7b629b7010d2d5c534022271444f2406
sergeykotenko <sergeykotenko@bk.ru>
2021-12-01 23:56:27 +0300

    before vue3block

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,25 +1,64 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+<!--        <div class="row justify-content-center">-->
+<!--            <div class="col-md-8">-->
+<!--                <div class="card">-->
+<!--                    <div class="card-header">Example Component</div>-->
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                    <a href="/register">Регистрация</a>
-                    <a href="/login">Логин</a>
-                </div>
-            </div>
+<!--                    <div class="card-body">-->
+<!--                        I'm an example component.-->
+<!--                    </div>-->
+<!--                    <a href="/register">Регистрация</a>-->
+<!--                    <a href="/login">Логин</a>-->
+<!--                </div>-->
+<!--            </div>-->
+
+<!--            <div>-->
+<!--                asdasdas-->
+<!--                <div></div>-->
+<!--            </div>-->
+
+<!--        </div>-->
+
+
+        <div>
+            <tree :tree-data="tree"></tree>
         </div>
+
     </div>
 </template>
 
 <script>
+    // export default {
+    //     mounted() {
+    //         console.log('Component mounted.')
+    //     }
+    // }
+
+    import Tree from './Tree';
+
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        data: () => ({
+            tree: {
+                label: 'A cool folder',
+                children: [
+                    {
+                        label: 'A cool sub-folder 1',
+                        children: [
+                            { label: 'A cool sub-sub-folder 1',
+                                children: [
+                                    { label: 'newwww', }
+                                ]
+                            },
+                            { label: 'A cool sub-sub-folder 2' }
+                        ]
+                    },
+                    { label: 'This one is not that cool' }
+                ]
+            }
+        }),
+        components: {
+            Tree
         }
-    }
+    };
 </script>
---
+++ resources/js/components/Tree.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container">
+        <!--        <div class="row justify-content-center">-->
+        <!--            <div class="col-md-8">-->
+        <!--                <div class="card">-->
+        <!--                    <div class="card-header">Example Component</div>-->
+
+        <!--                    <div class="card-body">-->
+        <!--                        I'm an example component.-->
+        <!--                    </div>-->
+        <!--                    <a href="/register">Регистрация</a>-->
+        <!--                    <a href="/login">Логин</a>-->
+        <!--                </div>-->
+        <!--            </div>-->
+
+        <!--            <div>-->
+        <!--                asdasdas-->
+        <!--                <div></div>-->
+        <!--            </div>-->
+
+        <!--        </div>-->
+
+
+        <div class="tree">
+            <ul class="tree-list">
+                <node-tree :node="treeData"></node-tree>
+            </ul>
+        </div>
+
+    </div>
+</template>
+
+<script>
+// export default {
+//     mounted() {
+//         console.log('Component mounted.')
+//     }
+// }
+
+import NodeTree from './NodeTree';
+
+export default {
+    props: {
+        treeData: Object
+    },
+    components: {
+        NodeTree
+    }
+};
+</script>
+
+
+<style>
+.tree-list ul {
+    margin: 6px 0;
+    padding-left: 16px;
+}
+</style>
